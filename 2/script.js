63b4898206b4c3b9dda60fe174068a8fe9a9b3e7
Raúl Morales <raulmoralesruiz@outlook.com>
2020-09-19 20:30:15 +0200

    corregido ej2. (apartado insertar empleado)

--- 2/script.js
+++ 2/script.js
@@ -2,26 +2,34 @@
 Está página permitirá:
 
 • Insertar nuevos empleados en la tabla.
-    Deberá crear la función javascriptcorrespondiente que permita insertar nuevos empleados.
-    En el caso de insertarun nuevo empleado deberá actualizarse el número total de empleados.
-    En elcaso de ya existir el empleado deberá mostrarse una alerta indicando que yaexiste el empleado.
+    Deberá crear la función javascript correspondiente que permita insertar nuevos empleados.
+    En el caso de insertar un nuevo empleado deberá actualizarse el número total de empleados.
+    En el caso de ya existir el empleado deberá mostrarse una alerta indicando que ya existe el empleado.
     El DNI será el identificador del empleado.
 
 • Borrar un empleado de la tabla.
-    Deberá crear la función javascriptcorrespondiente que permita borrar un empleado.
-    Para borrar un empleadodeberá introducirse el DNI del empleado que se desea borrar.
+    Deberá crear la función javascript correspondiente que permita borrar un empleado.
+    Para borrar un empleado deberá introducirse el DNI del empleado que se desea borrar.
     En el caso deborrar un empleado deberá actualizarse el número total de empleados.
-    En elcaso de que no exista el empleado deberá mostrarse una alerta.
-    
+    En el caso de que no exista el empleado deberá mostrarse una alerta.
+
 • Modificar un empleado de la tabla.
-    Deberá crear la función javascriptcorrespondiente.
-    En el caso de que no exista el empleado a modificar deberámostrar una alerta indicando que el empleado a modificar no existe.
+    Deberá crear la función javascript correspondiente.
+    En el caso de que no exista el empleado a modificar deberá mostrar una alerta indicando que el empleado a modificar no existe.
 */
 
+
+
+//ubicación de línea que muestra el número de empleados
 const numEmp = document.getElementById("num-emp");
+
+//tabla que almacena todos los datos
 const tabla = document.getElementById("tabla-emp");
 
+//se obtiene el número de empleados (número total de filas - 1)
 let totalEmpleados = tabla.rows.length - 1;
+
+//se actualiza la línea anterior con el número actual de empleados
 numEmp.innerText = "Número total de empleados: " + totalEmpleados;
 
 
@@ -39,6 +47,7 @@ function insertar() {
 
     let celdaDNI = document.createElement("td");
     fila.appendChild(celdaDNI);
+    celdaDNI.classList.add("dniEmp");
 
     let celdaNom = document.createElement("td");
     fila.appendChild(celdaNom);
@@ -47,23 +56,28 @@ function insertar() {
     fila.appendChild(celdaApe);
 
 
+
     //se piden los datos
     let posEmp = totalEmpleados + 1;
     console.log(posEmp);
 
+
     let dni = prompt("DNI del empleado:");
     console.log(dni);
 
+    //mientras el dni exista, se volverá a pedir (hasta que no exista).
+    while (empleadoExiste(dni) == true) {
+        dni = prompt("El empleado ya existe, introduce otro dni.");
+    }
+
+
     let nombre = prompt("Nombre del empleado:");
     console.log(nombre);
-    if (empleadoExiste(nombre)) {
-        alert("el empleado ya existe");
-    } //else {
-    //    alert("el empleado NO existe");
-    //}
+
 
     let ape = prompt("Apellidos del empleado:");
     console.log(ape);
+
 
 
     // se incluyen los datos en cada celda
@@ -80,19 +94,19 @@ function modificar() {
 }
 
 
+
 function empleadoExiste(emp) {
-    let empleados = document.getElementsByTagName("td");
-    let resul = 0;
+    //guarda todos las celdas con clase dniEmp en la variable empleados
+    let empleados = document.getElementsByClassName("dniEmp");
+
     let encontrado = false;
 
+    //buscamos si el empleado existe (cualquier celda de la tabla)
     for (let i = 0; i < empleados.length && encontrado != true; i++) {
-
-        if (emp == empleados[i].innerText) {
-            resul = 1;
+        if (emp == empleados[i].textContent) {
             encontrado = true;
         }
-
-        // console.log(empleados[i].innerText);
     }
 
+    return encontrado;
 }
